80fe9d460ad78c0c4ec5f593cc9dc249f7f3a10f
Kirill <kirill040605@gmail.com>
2024-01-07 10:31:24 -0800

    SinglePage Component no longer used

--- src/components/pages/SinglePage/SinglePage.js
+++ src/components/pages/SinglePage/SinglePage.js
@@ -1,55 +1,53 @@
+// import './singlePage.scss';
+// import { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import RequestService from '../../../services/RequestService';
+// import Spinner from '../../spinner/Spinner';
+// import ErrorMessage from '../../errorMessage/ErrorMessage';
 
-import './singlePage.scss';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import RequestService from '../../../services/RequestService';
-import Spinner from '../../spinner/Spinner';
-import ErrorMessage from '../../errorMessage/ErrorMessage';
 
-
-const SinglePage = ({Component, currentSearch}) => {
-    const {id} = useParams();
-    const [data, setData] = useState(null);
-    const {loading, error, clearError, getDataById, getExternalId} = RequestService();
+// const SinglePage = ({Component, currentSearch}) => {
+//     const {id} = useParams();
+//     const [data, setData] = useState(null);
+//     const {loading, error, clearError, getDataById, getExternalId} = RequestService();
     
-    useEffect(() => {
-        //queueMicrotask(updateData);
-        updateData();
-    }, [id])
+//     useEffect(() => {
+//         updateData();
+//     }, [id])
 
-    useEffect(() => {
-        console.log(data);
-    }, [data])
+//     useEffect(() => {
+//         console.log(data);
+//     }, [data])
     
-    const updateData = async () => {
-        clearError();
+//     const updateData = async () => {
+//         clearError();
         
-        await getExternalId(id, currentSearch)
-            .then(externalId => getDataById(externalId.imdb_id, 'imdb_id', currentSearch) )
-            .then(data => onDataLoaded(data[`${currentSearch}_results`][0]));
-    }
+//         await getExternalId(id, currentSearch)
+//             .then(externalId => getDataById(externalId.imdb_id, 'imdb_id', currentSearch) )
+//             .then(data => onDataLoaded(data[`${currentSearch}_results`][0]));
+//     }
 
     
 
-    const onDataLoaded = (data) => setData(data)
+//     const onDataLoaded = (data) => setData(data)
 
-    const spinner = loading ? <Spinner/> : null;
-    const errorMessage = error ? <ErrorMessage/> : null;
-    const content = !(loading || error || !data) ? <Component data={data}/> : null
+//     const spinner = loading ? <Spinner/> : null;
+//     const errorMessage = error ? <ErrorMessage/> : null;
+//     const content = !(loading || error || !data) ? <Component data={data}/> : null
 
-    return (
-        <>
-            {spinner}
-            {errorMessage}
-            {  
-                currentSearch !== 'movie' ? content : null
-            }
-        </>
+//     return (
+//         <>
+//             {spinner}
+//             {errorMessage}
+//             {  
+//                 content
+//             }
+//         </>
         
-    )
-}
+//     )
+// }
 
 
 
 
-export default SinglePage;
+// export default SinglePage;
